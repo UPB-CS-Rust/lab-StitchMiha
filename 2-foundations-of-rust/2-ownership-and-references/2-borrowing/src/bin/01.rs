d56345d69e29acbef0e42b303405b24f4777aae7
StitchMiha <mihadimovska@gmail.com>
2024-10-24 19:22:37 +0300

    Lab 02 Exercises

--- 2-foundations-of-rust/2-ownership-and-references/2-borrowing/src/bin/01.rs
+++ 2-foundations-of-rust/2-ownership-and-references/2-borrowing/src/bin/01.rs
@@ -3,9 +3,10 @@
 
 fn main() {
     let mut x = 100;
-    let y = &mut x;
+    let y = &mut x; // both  y and z are mutable references to x
+    *y += 100; // we dropped it here the reference before using it again to create z
     let z = &mut x;
-    *y += 100;
+// we can only have one mutable reference to a variable
     *z += 1000;
     assert_eq!(x, 1200);
 }
